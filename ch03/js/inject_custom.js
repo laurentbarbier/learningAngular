cadea36ea9a85d2b7b37080e76c79a91f0b1f777
Laurent_Barbier <barbier.laurent@gmail.com>
2015-04-05 09:10:04 +0200

    Add injection between 2 modules

--- ch03/js/inject_custom.js
+++ ch03/js/inject_custom.js
@@ -1,4 +1,13 @@
 /**
  * Created by Laurent_Barbier on 05/04/2015.
  */
-
+var myMod = angular.module('myMod', []);
+myMod.value('modMsg', 'Hello from My Module');
+myMod.controller('controllerB', ['$scope', 'modMsg', function($scope, msg) {
+    $scope.message = msg;
+}]);
+var myApp = angular.module('myApp', ['myMod']);
+myApp.value('appMsg', 'Hello from My App');
+myApp.controller('controllerA', ['$scope', 'appMsg', function($scope, msg) {
+    $scope.message = msg;
+}]);
