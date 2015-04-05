ce2a8f6ba6f9dde3d5630615002d275466e25f29
Laurent_Barbier <barbier.laurent@gmail.com>
2015-04-05 17:29:32 +0200

    ch06 ng-include usage

---
+++ ch06/js/directive_angular_include.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Laurent_Barbier on 05/04/2015.
+ */
+angular.module('myApp', []).
+    controller('myController', function($scope) {
+       $scope.titleBar = 'small_title.html';
+    });
