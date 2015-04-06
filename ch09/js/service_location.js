e89c1cb0c48474cd1a3501d5a6a4fbbfdf50f166
Laurent_Barbier <barbier.laurent@gmail.com>
2015-04-06 21:45:30 +0200

    ch09 service location

---
+++ ch09/js/service_location.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Laurent_Barbier on 06/04/2015.
+ */
+angular.module('myApp', [])
+    .controller('myController', ['$scope', '$location', function ($scope, location) {
+        $scope.updateLocationInfo = function() {
+            $scope.url = location.url();
+            $scope.absUrl = location.absUrl();
+            $scope.host = location.host();
+            $scope.port = location.port();
+            $scope.protocol = location.protocol();
+            $scope.path = location.path();
+            $scope.search = location.search();
+            $scope.hash = location.hash();
+        };
+
+        $scope.changePath = function () {
+            location.path("/new/path");
+            $scope.updateLocationInfo();
+        };
+
+        $scope.changeHash = function () {
+            location.path("newHash");
+            $scope.updateLocationInfo();
+        };
+
+        $scope.changeSearch = function () {
+            location.search("p1", "newA");
+            $scope.updateLocationInfo();
+        };
+
+        $scope.updateLocationInfo();
+    }]);
