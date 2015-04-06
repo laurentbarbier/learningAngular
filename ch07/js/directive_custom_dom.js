0b140309d0b8d277094af2c2fea8b0e77d23d538
Laurent_Barbier <barbier.laurent@gmail.com>
2015-04-06 15:59:37 +0200

    ch07 Custom directive that manipulates the DOM

---
+++ ch07/js/directive_custom_dom.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Laurent_Barbier on 06/04/2015.
+ */
+angular.module('myApp', [])
+    .controller('myController', function ($scope) {
+        $scope.title = "myApplication";
+    })
+    .directive('mybox', function () {
+        return {
+            transclude: true,
+            restrict: 'E',
+            scope: {title: '@', bwidth: '@'},
+            template: '<div><span class="titleBar">{{title}}</span>' +
+            '<div ng-transclude></div></div>',
+            link: function (scope, elem, attr, controller, transclude) {
+                elem.append('<span class="footer">' + scope.$parent.title + '</span>');
+                elem.css('border', '2px ridge black');
+                elem.css('display', 'inline-block');
+                elem.css('width', scope.bwidth);
+            },
+        };
+    });
