0834618997e7e179d64358cfa9b30661335a5777
Laurent_Barbier <barbier.laurent@gmail.com>
2015-04-11 18:42:59 +0200

    ch11 Ratings stars

---
+++ ch11/js/rating.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Laurent_Barbier on 11/04/2015.
+ */
+angular.module('myApp', [])
+    .controller('myController', ['$scope', function ($scope) {
+        $scope.stars = [1, 2, 3, 4, 5];
+        $scope.items = [
+            {description: "Delicate Arch", img: "/images/arch.jpg", rating: 3},
+            {description: "Silver Lake", img: "/images/lake.jpg", rating: 4},
+            {description: "Yellowstone Bison", img: "/images/bison.jpg", rating: 2}
+        ];
+        $scope.adjustRating = function (item, value) {
+            item.rating = value;
+        };
+    }]);
